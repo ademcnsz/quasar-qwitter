fbffbd7763478159ce89aa49934d03ae43b94899
Adem CANSIZ <ademmcansiz@gmail.com>
2023-02-28 09:23:30 +0300

    frontend complete

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,60 +1,62 @@
 <template>
-  <q-page class="">
-    <div class="q-pa-lg row items-end q-col-gutter-md">
-        <div class="col">
-          <q-input bottom-slots autogrow class="newTweetInput" v-model="newTweetContent" placeholder ="Neler oluyor?" counter maxlength="280" :dense="dense">
-          <template v-slot:before>
-            <q-avatar size="xl">
-              <img src="https://pbs.twimg.com/profile_images/1579971252277723136/6viwe3VO_400x400.jpg">
-            </q-avatar>
-          </template>
+  <q-page class="relative-position">
+    <q-scroll-area class="absolute fullscreen">
+      <div class="q-pa-lg row items-end q-col-gutter-md">
+          <div class="col">
+            <q-input bottom-slots autogrow class="newTweetInput" v-model="newTweetContent" placeholder ="Neler oluyor?" counter maxlength="280" :dense="dense">
+            <template v-slot:before>
+              <q-avatar size="xl">
+                <img src="https://pbs.twimg.com/profile_images/1579971252277723136/6viwe3VO_400x400.jpg">
+              </q-avatar>
+            </template>
 
 
-        </q-input></div>
-        <div class="col col-shrink">
-            <q-btn @click="addNewTweet" unelevated rounded color="primary" class="q-mb-md" label="Tweetle" no-caps :disable="newTweetContent"/>
+          </q-input></div>
+          <div class="col col-shrink">
+              <q-btn @click="addNewTweet" unelevated rounded color="primary" class="q-mb-md" label="Tweetle" no-caps :disable="newTweetContent"/>
+          </div>
         </div>
-      </div>
-      <q-separator color="grey-2 border border-primary" size="15px" class="divider"/>
+        <q-separator color="grey-2 border border-primary" size="15px" class="divider"/>
 
-      <q-list separator>
-        <transition-group
-          appear
-          enter-active-class="animated fadeIn slow"
-          leave-active-class="animated fadeOut slow"
-        >
-          <q-item v-for="tweet in tweets" :key="tweet.date" clickable v-ripple class="tweet q-py-md" >
-          <q-item-section avatar top>
-            <q-avatar>
-              <img src="https://pbs.twimg.com/profile_images/1579971252277723136/6viwe3VO_400x400.jpg">
-            </q-avatar>
-          </q-item-section>
+        <q-list separator>
+          <transition-group
+            appear
+            enter-active-class="animated fadeIn slow"
+            leave-active-class="animated fadeOut slow"
+          >
+            <q-item v-for="tweet in tweets" :key="tweet.date" clickable v-ripple class="tweet q-py-md" >
+            <q-item-section avatar top>
+              <q-avatar>
+                <img src="https://pbs.twimg.com/profile_images/1579971252277723136/6viwe3VO_400x400.jpg">
+              </q-avatar>
+            </q-item-section>
 
-          <q-item-section>
-            <q-item-label ><strong>Adem Cansız</strong>
-              <span class="text-grey-7 q-ml-sm ">@ademmcansiz</span>
-            </q-item-label>
-            <q-item-label  >
-              {{ tweet.content }}
-            </q-item-label>
-            <div class="row justify-between q-mt-md">
-               <q-btn flat round color="grey" size="sm" icon="fa-solid fa-comment"></q-btn>
-               <q-btn flat round color="grey" size="sm" icon="fa-solid fa-retweet"></q-btn>
-               <q-btn flat round color="grey" size="sm" icon="fa-regular fa-heart"></q-btn>
-               <q-btn @click="deleteTweet(tweet)" flat round color="grey" size="sm" icon="fa-solid fa-trash"></q-btn>
-            </div>
-          </q-item-section>
+            <q-item-section>
+              <q-item-label ><strong>Adem Cansız</strong>
+                <span class="text-grey-7 q-ml-sm ">@ademmcansiz</span>
+              </q-item-label>
+              <q-item-label  >
+                {{ tweet.content }}
+              </q-item-label>
+              <div class="row justify-between q-mt-md">
+                <q-btn flat round color="grey" size="sm" icon="fa-solid fa-comment"></q-btn>
+                <q-btn flat round color="grey" size="sm" icon="fa-solid fa-retweet"></q-btn>
+                <q-btn flat round color="grey" size="sm" icon="fa-regular fa-heart"></q-btn>
+                <q-btn @click="deleteTweet(tweet)" flat round color="grey" size="sm" icon="fa-solid fa-trash"></q-btn>
+              </div>
+            </q-item-section>
 
-          <q-item-section side top>
-            <!-- {{ formatTime(tweet.date)  }} -->
-            {{ tweet.date  }}
-          </q-item-section>
-          </q-item>
-        </transition-group>
-        <q-separator inset="item" />
+            <q-item-section side top>
+              <!-- {{ formatTime(tweet.date)  }} -->
+              {{ tweet.date  }}
+            </q-item-section>
+            </q-item>
+          </transition-group>
+          <q-separator inset="item" />
 
 
-    </q-list>
+      </q-list>
+  </q-scroll-area>
   </q-page>
 </template>
 <!-- .divider
@@ -112,6 +114,6 @@ export default defineComponent({
   textarea
   font-size:19px
   line-height:1.4 !important
- 
+
 
 </style>
